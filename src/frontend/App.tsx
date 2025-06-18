6eb48a0a13045bd9569e06022edb9b0a9f7f3eb1
Yang <Theoneandonlyyang@gmail.com>
2025-06-18 12:57:51 -0700

    basic landing page UI with no styling

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -6,19 +6,15 @@ function App() {
     <>
         <div>
             <video
-                style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    width: '100vw',
-                    height: '100vh',
-                    objectFit: 'cover',
-                    zIndex: -1,
-                }}
                 autoPlay muted loop playsInline className="background-video">
                 <source src="/bgVideo.mp4" type="video/mp4"/>
                 Your browser doesn’t support the video tag.
             </video>
+            <div>
+                <h1>Title</h1>
+            </div>
+            <button className="auth-Button">Create an Account</button>
+            <button className="auth-Button">Sign in</button>
         </div>
     </>
   )
