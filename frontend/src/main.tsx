12ba20efce59d4331cb5c408855f0d046a5b3b8e
siddsaran <155814708+siddsaran@users.noreply.github.com>
2025-06-18 19:07:53 -0700

    Add files via upload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import MainPage from './MainPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MainPage />
   </StrictMode>,
 )
