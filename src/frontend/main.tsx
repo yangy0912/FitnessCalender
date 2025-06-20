1d2f8f03fe0736a3a7377a0c2802fb5c7e32a914
Siddharth Saran <siddharthsaran@MacBook-Pro-254.local>
2025-06-19 17:31:18 -0700

    delete irrelevant files

--- src/frontend/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import MainPage from './MainPage.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <MainPage />
-  </StrictMode>,
-)
