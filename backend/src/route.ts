9b134168f5d381780c814b83f30c08634d0c18dc
Siddharth Saran <siddharthsaran@MacBook-Pro-254.local>
2025-06-21 16:06:19 -0700

    added sample calendar layout

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -15,11 +15,39 @@ export const signInRequest = (req: SafeRequest, res: SafeResponse) => {
 export const createAccRequest = (req: SafeRequest, res: SafeResponse) => {
     // Supress warnings
     console.log("Create Account request received at:", req.url);
+    testDBAddMatrix();
     res.json({ status: "success", message: "Account created" });
+}
+
+export const getUserData =  async (req: SafeRequest, res: SafeResponse) => {
+    const userID = req.query.id;
+    console.log(userID);
+    console.log(typeof userID);
+    if (typeof userID === 'string') {
+        const calenderData = db.collection("users").doc(userID);
+        const doc = await calenderData.get();
+        if (doc.exists) {
+            res.status(200).json(doc);
+        } else {
+            res.status(200).json({});
+        }
+    } else {
+        res.status(404).send({status: "User Not Found"});
+    }
 }
 
 const testDBchange = async (): Promise<any> => {
     await db.collection('users').doc('test').set({
-        data: 'Change it to sidd',
+        data: 'Change it to Yang',
     });
 };
+
+const testDBAddMatrix = async (): Promise<any> => {
+    await db.collection('users').doc('user_2ypnofjP8HiE3khsBWlsSfBfKuf').set({
+        data: {
+            "6/20" : ["a", "b"],
+            "6/21" : ["c", "d"],
+            "6/22" : ["e", "f"]
+        }}, { merge: true }
+    );
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,56 @@
-import { LandingPage } from "./LandingPage.tsx";
-import { useUser } from '@clerk/clerk-react';
-import { useEffect } from "react";
-import { SignedOut, SignedIn } from "@clerk/clerk-react";
-import { CalendarTest } from "./CalendarTest.tsx";
+import {LandingPage} from "./LandingPage.tsx";
+import {CalendarTest} from "./CalendarTest.tsx";
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-react'
+import {useEffect, useState} from "react";
 
-
+type DataMap = { [date: string]: string[] } | undefined;
 
 function App() {
+
     const { user, isSignedIn, isLoaded } = useUser();
+    const [userData, setUserData] = useState<DataMap | undefined>(undefined);
+
+    const fetchUserData = async () : Promise<DataMap> => {
+        try {
+            if (user === null || user === undefined) {
+                throw new Error("No user found");
+            } else {
+                const res = await fetch(`http://localhost:8088/api/getUserData?id=${user.id}`);
+                const userData = await res.json();
+                console.log('Received:', userData);
+                return userData;
+            }
+        } catch (error) {
+            console.error('Fetch error:', error);
+            return undefined;
+        }
+    };
 
     useEffect(() => {
         if (isLoaded && isSignedIn && user) {
-            console.log("User ID")
-            console.log(user.id)
+            (async () => {
+                const data = await fetchUserData();
+                setUserData(data);
+            })();
         }
     }, [isLoaded, isSignedIn, user])
 
+
+
     const doCreateAcc = () : void => {
         console.log("onclick DCA");
-        console.log("Before useEffect")
-
+        fetch("http://localhost:8088/api/createAcc")
+            .then (res => res.json())
+            .then(json => console.log(json))
+            .catch(error => console.log(error));
     }
 
     const doSignIn = () : void => {
         console.log("onclick DSI");
-        console.log("Before useEffect")
+        fetch("http://localhost:8088/api/signIn")
+            .then (res => res.json())
+            .then(json => console.log(json.status))
+            .catch(error => console.log(error));
     }
 
     return (
@@ -33,10 +59,10 @@ function App() {
             <LandingPage onSignInClick={doSignIn} onCreateClick={doCreateAcc} />
         </SignedOut>
         <SignedIn>
-            
-            <CalendarTest />
+            <CalendarTest userID={user?.id} userName={user?.username} data={userData}
+                          userFirstName={user?.firstName} userLastName={user?.lastName} />
         </SignedIn>
-        
+
     </>
   )
 
